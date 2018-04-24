fb442ad1f727550537589ed9ec740e5f9b02900f
Jochen Singer <singerj@lo-login-01.leonhard.ethz.ch>
2018-04-24 09:53:48 +0200

    [FIX] Added missing variables.

--- src/findBestTrees.cpp
+++ src/findBestTrees.cpp
@@ -1,4 +1,4 @@
-/**
+false/**
  * SCIPhI: Single-cell mutation identification via phylogenetic inference
  * <p>
  * Copyright (C) 2018 ETH Zurich, Jochen Singer
@@ -102,8 +102,10 @@ int readParameters(Config<TTreeType> & config, int argc, char* argv[]){
 		("ms", boost::program_options::value<decltype(config.minSupport)>(&config.minSupport), "Minimum number of reads required to support the alternative. [0]")
 		("mf", boost::program_options::value<decltype(config.minFreq)>(&config.minFreq), "Minimum required frequency of reads supporting the alternative per cell. [0]")
 		("mff", boost::program_options::value<decltype(config.meanFilter)>(&config.meanFilter), "Mean of acceptable variant allele frequency across all cells for a specific locus. [0.25]")
-		("bns", boost::program_options::value<decltype(config.maxSupInControlBulk)>(&config.minSupport), "Loci with up to this number of alternative supporting reads in the bulk control sample will be skiped. [2]")
-		("bnc", boost::program_options::value<decltype(config.minCovInControlBulk)>(&config.minCoverage), "Minimum required coverage of reads in the bulk control sample. [6]")
+		("bns", boost::program_options::value<decltype(config.maxSupInControlBulk)>(&config.maxSupInControlBulk), "Loci with up to this number of alternative supporting reads in the bulk control sample will be skiped. [2]")
+		("bnc", boost::program_options::value<decltype(config.minCovInControlBulk)>(&config.minCovInControlBulk), "Minimum required coverage of reads in the bulk control sample. [6]")
+		("mnc", boost::program_options::value<decltype(config.maxNumberNormalCellMutated)>(&config.maxNumberNormalCellMutated), "Maximum number of control cells allowed to be mutated. [0]")
+		("unc", boost::program_options::value<decltype(config.useNormalCellsInTree)>(&config.useNormalCellsInTree), "Use normal cells for tree reconstruction. [false]")
 		//("cms", boost::program_options::value<decltype(config.computeMixScore)>(&config.computeMixScore), "Compute the mix score = allow a mutation to be homozygous or lost in a subtree.") - feature still in experimental phase
 		//("mspa", boost::program_options::value<double>(&config.clamPrior[0]), "The alpha parameter of the beta function used as prior on the rate of loosing a chromosome after some mutation occurred.") - feature still in experimental phase
 		//("mspb", boost::program_options::value<double>(&config.clamPrior[1]), "The beta parameter of the beta function used as prior on the rate of loosing a chromosome after some mutation occurred.") - feature still in experimental phase
