ecd49d7f9b60d937a5b75c0a813fe19545e7a35f
Jochen Singer <singerj@lo-login-01.leonhard.ethz.ch>
2018-10-03 22:24:16 +0200

    [FEATURE] Added a new tool to compare two trees.

--- benchmark/computeTreeDistanceGT.py
+++ benchmark/computeTreeDistanceGT.py
@@ -6,42 +6,27 @@ random.seed(42)
 
 inFileNameTruth = sys.argv[1]
 inFileNameInf = sys.argv[2]
+randomize = sys.argv[3]
 
-def computeCellDist(values, gts):
-    numCells = len(gts)
+def computeCellDist(cell1, cell2):
+    dist = []
+    for i in range(len(cell1)):
+        dist.append(abs(cell1[i] - cell2[i]))
+    return dist
+
+def computeSquareDist(dist1, dist2):
+    dist = 0
+    for i in range(len(dist1)):
+        dist += pow(dist1[i] - dist2[i], 2)
+    return float(dist / len(dist1))
+
+def computeTreeDist(distValues):
+    numCells = len(distValues)
+    dist = 0
     for i in range(numCells):
         for j in range(i + 1, numCells):
-            values[i][j] += abs(gts[i] - gts[j])
-
-def normalize(values, numMuts):
-    numCells = len(values)
-    for i in range(numCells):
-        for j in range(i + 1, numCells):
-            values[i][j] = float(values[i][j])/float(numMuts)
-
-def computeDist(valuesTruth, valuesInf):
-    numCells = len(valuesTruth)
-    result = 0
-    for i in range(numCells):
-        for j in range(i + 1, numCells):
-            result += pow(valuesTruth[i][j] - valuesInf[i][j], 2)
-    return math.sqrt(float(result/((numCells - 1) * (numCells) / 2)))
-    #return (float(result/((numCells - 1) * (numCells) / 2)))
-
-inFileTruth = open(inFileNameTruth, 'r')
-numCells = len(inFileTruth.readline().strip().split("\t")) -1
-truthValues = [[0 for i in range(numCells)] for j in range(numCells)]
-numMuts = 0
-for line in inFileTruth:
-    numMuts += 1
-    lineSplit = line.strip().split("\t")[1:]
-    gts = list(map(lambda x:int(x != '0'), lineSplit))
-    computeCellDist(truthValues, gts)
-normalize(truthValues, numMuts)
-
-
-#for i in range(len(truthValues)):
-#    print(truthValues[i])
+            dist += distValues[i][j]
+    return (float(math.sqrt(dist/((numCells - 1) * (numCells) / 2))))
 
 def shuffleGT(gts, randomOrder):
     temp = gts
@@ -50,24 +35,58 @@ def shuffleGT(gts, randomOrder):
     return temp
 
 inFileInf = open(inFileNameInf, 'r')
-inFileInf.readline()
+numCells = len(inFileInf.readline().strip().split("\t")) - 2
+if(randomize == '1'):
+    numCells += 1
+
 infValues = [[0 for i in range(numCells)] for j in range(numCells)]
+infGT = [ [] for i in range(numCells)]
+infDict = {}
 numMuts = 0
 randomOrder = [i for i in range(numCells)]
-#print(randomOrder)
 random.shuffle(randomOrder)
-#print(randomOrder)
 for line in inFileInf:
     numMuts += 1
-    #lineSplit = line.strip().split("\t")[2:]
-    #gts = list(map(lambda x: float(x.split("|")[2]), lineSplit))
-    lineSplit = line.strip().split("\t")[1:]
-    gts = list(map(lambda x: int(x != '0'), lineSplit))
-    gts = shuffleGT(gts, randomOrder)
-    computeCellDist(infValues, gts)
-normalize(infValues, numMuts)
 
-#for i in range(len(infValues)):
-#    print(infValues[i])
+    if(randomize == '1'):
+        lineSplit = line.strip().split("\t")
+        lineSplit[0] = str(numMuts)
+        gts = list(map(lambda x:float(x != '0'), lineSplit[1:]))
+        gts = shuffleGT(gts, randomOrder)
 
-print(computeDist(truthValues, infValues))
+    else:
+        lineSplit = line.strip().split("\t")[1:]
+        gts = list(map(lambda x: float(x.split("|")[2]), lineSplit[1:]))
+    
+    infDict[lineSplit[0]] = gts
+
+for key, value in sorted(infDict.items()):
+    for i in range(numCells):
+        infGT[i].append(value[i])
+
+inFileTruth = open(inFileNameTruth, 'r')
+truthGT = [ [] for i in range(numCells)]
+truthValues = [[0 for i in range(numCells)] for j in range(numCells)]
+truthDict = {}
+numMuts = 0
+for line in inFileTruth:
+    numMuts += 1
+    lineSplit = line.strip().split("\t")
+    if(randomize == '1'):
+        lineSplit[0] = str(numMuts)
+    gts = list(map(lambda x:float(x != '0'), lineSplit[1:]))
+    if lineSplit[0] in infDict:
+        truthDict[lineSplit[0]] = gts     
+        
+for key, value in sorted(truthDict.items()):
+    for i in range(numCells):
+        truthGT[i].append(value[i])
+
+distValues = [[0 for i in range(numCells)] for j in range(numCells)]
+for i in range(numCells):
+    for j in range(i + 1, numCells):
+        dist1 = computeCellDist(truthGT[i], truthGT[j])
+        dist2 = computeCellDist(infGT[i], infGT[j])
+        distValues[i][j] = computeSquareDist(dist1, dist2 )
+
+print(computeTreeDist(distValues))
